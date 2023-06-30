8e9ac94adb7963373a9d3fc6a3934be1cf8be752
WongGinnMunn <111555087+WongGinnMunn@users.noreply.github.com>
2023-06-30 14:48:12 +0800

    Added get_os_type file

---
+++ get_os_type.py
@@ -0,0 +1,31 @@
+import boto3
+import json
+
+ssm_client = boto3.client("ssm")
+
+def get_os_type(event):
+    try:
+        instance_info = ssm_client.describe_instance_information(
+            InstanceInformationFilterList=[
+                {"key": "InstanceIds",
+                    "valueSet": [event['InstanceId']]}
+            ]
+        )
+        if len(instance_info["InstanceInformationList"]) > 0:
+            os_type = instance_info["InstanceInformationList"][0]["PlatformType"]
+            event['OS_type'] = os_type
+            return event
+        else:
+            raise Exception(
+                "Please give permission to Systems Manager")
+    except Exception as e:
+        raise Exception(e)
+
+
+
+'''
+{
+  "InstanceId": "i-0d048bb574834bfa1",
+  "OS_type": "Linux"
+}
+'''
