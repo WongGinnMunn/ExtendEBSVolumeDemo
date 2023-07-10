17383be020e9257dcb50746a313e0ed50838b36b
WongGinnMunn <111555087+WongGinnMunn@users.noreply.github.com>
2023-07-10 16:52:58 +0800

    Update createjsonfile.py

--- createjsonfile.py
+++ createjsonfile.py
@@ -1,10 +1,10 @@
 import json
-
+import sys
 def main(events):
   my_dict = {
-    "InstanceId": events["InstanceId"]
+          "InstanceId": events[0]
   }
-  
+
   json_string = json.dumps(my_dict, indent=2)
   with open('temp.json', 'w') as f:
     f.write(json_string)
