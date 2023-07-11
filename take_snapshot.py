ad502eeac21003511d6c6b5b4bd8b5787906196c
WongGinnMunn <111555087+WongGinnMunn@users.noreply.github.com>
2023-07-11 13:12:04 +0800

    Create take_snapshot.py

---
+++ take_snapshot.py
@@ -0,0 +1,36 @@
+import json
+import sys
+import boto3
+
+ec2_client = boto3.client('ec2', 'us-east-1')
+
+
+def main(arg):
+    with open('InstanceData.json', 'r+') as file:
+        file_data = json.load(file)
+        
+        response = ec2_client.describe_snapshots(
+            SnapshotIds=[
+                file_data['SnapshotId']
+            ],
+        )
+        while (response['Snapshots'][0]['State'] != 'completed'):
+
+            response = ec2_client.describe_snapshots(
+                SnapshotIds=[
+                    file_data['SnapshotId']
+                ],
+            )
+        
+        toAppend = {"Snapshot_Status": response['Snapshots'][0]['State']}
+        
+        file_data.update(toAppend)
+        file.seek(0)
+        json.dump(file_data, file, indent=4)
+            
+            
+        return arg
+
+
+if __name__ == '__main__':
+    main(sys.argv[1:])
