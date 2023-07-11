843cade4cbb48689b91161d687fa2accf56b8c37
WongGinnMunn <111555087+WongGinnMunn@users.noreply.github.com>
2023-07-11 12:41:38 +0800

    Update get_os_type.py

--- get_os_type.py
+++ get_os_type.py
@@ -4,11 +4,12 @@ import sys
 
 ssm_client = boto3.client("ssm", 'us-east-1')
 
+
 def main(arg):
-    with open('InstanceData.json','r+') as file:
+    with open('InstanceData.json', 'r+') as file:
 
         file_data = json.load(file)
-        
+
         try:
             instance_info = ssm_client.describe_instance_information(
                 InstanceInformationFilterList=[
@@ -19,14 +20,14 @@ def main(arg):
 
             if len(instance_info["InstanceInformationList"]) > 0:
                 os_type = instance_info["InstanceInformationList"][0]["PlatformType"]
-                
+
                 toAppend = {"OS_Type": os_type}
-                file_data.append(toAppend)
+                file_data.update(toAppend)
                 file.seek(0)
-                json.dump(file_data, file, indent = 4)
+                json.dump(file_data, file, indent=4)
 
                 return arg
-            
+
             else:
                 raise Exception(
                     "Please give permission to Systems Manager")
@@ -35,4 +36,4 @@ def main(arg):
 
 
 if __name__ == '__main__':
-  main(sys.argv[1:])
+    main(sys.argv[1:])
