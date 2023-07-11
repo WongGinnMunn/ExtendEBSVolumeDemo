4c0ffdbe26a53c019af86af1bedbdc14c5eff9dc
WongGinnMunn <111555087+WongGinnMunn@users.noreply.github.com>
2023-07-11 12:00:10 +0800

    Update hello_world.py

--- hello_world.py
+++ hello_world.py
@@ -4,8 +4,7 @@ import json
 def main(event):
   f = open('InstanceData.json')
   data = json.load(f)
-  for i in data['InstanceId']:
-    print(i)
+  print(data['InstanceId'])
 
 if __name__ == '__main__':
     main(sys.argv[1:])
