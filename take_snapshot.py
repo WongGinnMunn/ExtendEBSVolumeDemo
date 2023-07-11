148b5bda1862fc44f5e754e6299ca06fbce0f7d1
WongGinnMunn <111555087+WongGinnMunn@users.noreply.github.com>
2023-07-11 13:12:44 +0800

    Update take_snapshot.py

--- take_snapshot.py
+++ take_snapshot.py
@@ -2,35 +2,33 @@ import json
 import sys
 import boto3
 
-ec2_client = boto3.client('ec2', 'us-east-1')
+ec2 = boto3.client('ec2', 'us-east-1')
 
 
 def main(arg):
     with open('InstanceData.json', 'r+') as file:
         file_data = json.load(file)
         
-        response = ec2_client.describe_snapshots(
-            SnapshotIds=[
-                file_data['SnapshotId']
-            ],
-        )
-        while (response['Snapshots'][0]['State'] != 'completed'):
-
-            response = ec2_client.describe_snapshots(
-                SnapshotIds=[
-                    file_data['SnapshotId']
-                ],
+        try:
+            response = ec2.create_snapshot(
+                Description="a snapshot before the volume resizing",
+                VolumeId=file_data["VolumeId"]
             )
-        
-        toAppend = {"Snapshot_Status": response['Snapshots'][0]['State']}
-        
-        file_data.update(toAppend)
-        file.seek(0)
-        json.dump(file_data, file, indent=4)
-            
-            
-        return arg
 
-
+            toAppend = {"SnapshotId": response["SnapshotId"]}
+            
+            file_data.update(toAppend)
+            file.seek(0)
+            json.dump(file_data, file, indent=4)
+            
+            return arg
+        except Exception as e:
+            raise Exception(e)
+    
+    
 if __name__ == '__main__':
     main(sys.argv[1:])
+
+
+
+    #  include ec2: create tags permission
