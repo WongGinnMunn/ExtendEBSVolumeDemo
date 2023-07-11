ce09f58124e4f660b2fd606557910b3ba9cc9168
WongGinnMunn <111555087+WongGinnMunn@users.noreply.github.com>
2023-07-11 12:35:22 +0800

    Update get_os_type.py

--- get_os_type.py
+++ get_os_type.py
@@ -5,31 +5,34 @@ import sys
 ssm_client = boto3.client("ssm", 'us-east-1')
 
 def main(arg):
-    with open("InstanceData.json",'r+') as file:
-          # First we load existing data into a dict.
-        event = json.load(file)
-    
-    try:
-        instance_info = ssm_client.describe_instance_information(
-            InstanceInformationFilterList=[
-                {"key": "InstanceIds",
-                    "valueSet": [event['InstanceId']]}
-            ]
-        )
-        print("hello2")
-        if len(instance_info["InstanceInformationList"]) > 0:
-            os_type = instance_info["InstanceInformationList"][0]["PlatformType"]
-            
-            event.append({"OS_type":os_type})
-            file.seek(0)
-            json.dump(event, file, indent = 4)
+    with open('InstanceData.json','r+') as file:
 
-            return arg
-        else:
-            raise Exception(
-                "Please give permission to Systems Manager")
-    except Exception as e:
-        raise Exception(e)
+        file_data = json.load(file)
+        
+        try:
+            instance_info = ssm_client.describe_instance_information(
+                InstanceInformationFilterList=[
+                    {"key": "InstanceIds",
+                        "valueSet": [file_data['InstanceId']]}
+                ]
+            )
+
+            if len(instance_info["InstanceInformationList"]) > 0:
+                os_type = instance_info["InstanceInformationList"][0]["PlatformType"]
+                
+                toAppend = {"OS_Type": os_type}
+                file_data.append(toAppend)
+                file.seek(0)
+                json.dump(file_data, file, indent = 4)
+
+                return arg
+            
+            else:
+                raise Exception(
+                    "Please give permission to Systems Manager")
+        except Exception as e:
+            raise Exception(e)
+
 
 if __name__ == '__main__':
   main(sys.argv[1:])
